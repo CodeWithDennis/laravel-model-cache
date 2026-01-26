4718d5363b2419319a614ed01eddb3069de111e2
CodeWithDennis <personal.elsinga@gmail.com>
2026-01-26 18:09:58 +0100

    Basic Setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cache Pre Warming
---
+++ src/Example.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeWithDennis\CachePreWarming;
+
+/**
+ * @internal
+ */
+final class Example
+{
+    public function foo(): string
+    {
+        return 'bar';
+    }
+}
